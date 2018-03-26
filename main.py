36299148cb2de32a8336eaa489bc4146ee5ffa35
sharonWU0505 <astrumstella048@gmail.com>
2018-03-27 07:31:39 +0800

    Edit image type and size

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
+# %matplotlib inline
+# -*- coding: utf-8 -*-
 import sys
+import os
 import os.path
-import numpy as np
 from os import listdir
 from PIL import Image
+#from random import shuffle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
 from sklearn.decomposition import NMF
-from sklearn import svm
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
 
-
-def load_images(folder_path):
+def load_data(folder_path, class_dict):
     img_paths = listdir(folder_path)
-    pixels = []
-
-    for img_path in img_paths[:1000]:
-        path = os.path.join(folder_path, img_path)
-        if not img_path.startswith('.') and os.path.isfile(path):
-            img = Image.open(path).convert('RGB')
-            arr = np.array(img).astype('float32')
-            flat_arr = arr.ravel() / 255
-            pixels.append(flat_arr)
-
-    return np.array(pixels)
-
-
-def load_labels(folder_path):
-    img_paths = listdir(folder_path)
+    imgs = []
     labels = []
 
-    for img_path in img_paths[:1000]:
-        image_name = img_path[img_path.rfind("/") + 1: img_path.rfind("_")]
-        labels.append(float(class_dict[image_name]))
+    for img_path in img_paths:
+        # get images
+        path = os.path.join(folder_path, img_path)
+        if not img_path.startswith('.') and os.path.isfile(path):
+            img = Image.open(path).convert('LA')
+            imgs.append(np.array(img).ravel())
 
-    return labels
+        # get labels
+        image_name = img_path[img_path.rfind("/") + 1: img_path.rfind("_")]
+        try:
+            labels.append(float(class_dict[image_name]))  # turn to float?
+        except KeyError:
+            pass
+
+    return imgs, labels
 
 
 def NMF_decomposition(images):
-    model = NMF(n_components=256, init='nndsvda', random_state=0)
+    model = NMF(n_components=256, init='random', random_state=0)
     W = model.fit_transform(images)
     # H = model.components_
 
     return W
 
 
-# def SVD_decomposition(images):
-#
-#
-# def PCA_decomposition(images):
-
-
 if __name__ == "__main__":
-    dir = sys.argv[1]
-
     data_dict = {
-        0: "training",
-        1: "testing",
-        2: "padding_training",
-        3: "padding_testing"
+        0: "training_sun_rose",
+        1: "testing_sun_rose",
+        2: "training_dan_tul",
+        3: "testing_dan_tul",
+        4: "p_training_sun_rose",
+        5: "p_testing_sun_rose",
+        6: "p_training_dan_tul",
+        7: "p_testing_dan_tul"
     }
 
-    class_dict = {
-        "daisy": 0,
-        "dandelion": 1,
-        "rose": 2,
-        "sunflower": 3,
-        "tulip": 4
+    class_dict_sun_rose = {
+        "sunflower": 0,
+        "rose": 1
     }
 
-    # load training data and do decomposition
-    train_dir = dir + data_dict[2]
-    train_labels = load_labels(train_dir)
-    train_images = load_images(train_dir)
-    train_data = NMF_decomposition(train_images)
+    class_dict_dan_tul = {
+        "dandelion": 0,
+        "tulip": 1
+    }
 
-    # classify
-    classifier = svm.SVC(gamma=0.001)
-    classifier.fit(train_data, train_labels)
+    # load data
+    print(">>> Load data")
+    # sunflower and rose
+    training_paths = os.getcwd() + '/data/' + data_dict[0]
+    training_data, training_label = load_data(training_paths, class_dict_sun_rose)
+    testing_paths = os.getcwd() + '/data/' + data_dict[1]
+    testing_data, testing_label = load_data(testing_paths, class_dict_sun_rose)
 
-    test_dir = dir + data_dict[3]
-    test_data = NMF_decomposition(load_images(test_dir))
-    predicted = classifier.predict(test_data)
-    expected = load_labels(test_dir)
+    # # dandelion and tulip
+    # training_paths = os.getcwd() + '/data/' + data_dict[2]
+    # training_data, training_label = load_data(training_paths, class_dict_dan_tul)
+    # testing_paths = os.getcwd() + '/data/' + data_dict[3]
+    # testing_data, testing_label = load_data(testing_paths, class_dict_dan_tul)
 
-    print('predicted: ', predicted[:30])
-    print('expected: ', expected[:30])
+    print(training_data[0])
+    print("original shape: ", training_data[0].shape)
+    print("length of training data: ", len(training_data), len(training_label))
+
+    print(">>> Decomposition reduction")
+    X_train_reduced = NMF_decomposition(training_data)
+    print("shape after reduction: ", X_train_reduced[0].shape)
+
+    # classification
+    print(">>> Classification")
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X_train_reduced, training_label)
+
+    print(">>> Decompose testing data")
+    X_test_reduced = NMF_decomposition(testing_data)
+    score = clf.score(X_test_reduced, testing_label)
+    print("Score: ", score)
+
+    predicted = clf.predict(X_test_reduced)
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(testing_label, predicted))
